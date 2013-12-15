b78b4435f5678ca6a4417cdeba82cb7d14431c96
Thanh Tran <trongthanh@gmail.com>
2013-12-15 19:38:47 +0800

    Update and finish a job post html page, integrate Google analytics & utilize some front-end optimization techniques

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
 
-$(function () {
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+
+(function () {
 	'use strict';
 
 	//just for fun, showing Nau's ASCII art in browser console
-	var nauHeader = $('#nau-header').html();
+	var nauHeader = document.getElementById('nau-header').innerText;
 	console.log(nauHeader);
-});
+}());
