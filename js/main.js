de51696ccbe0966d1431a12b4392ed1081ac0418
Thanh Tran <trongthanh@gmail.com>
2014-02-14 18:07:18 +0700

    [WIP] Finish layout for desktop and landscape iPad

--- js/main.js
+++ js/main.js
@@ -22,9 +22,32 @@
 }());
 
 
-(function () {
+(function (doc) {
 	'use strict';
 
 	//just for fun, showing Nau's ASCII art in browser console
 	console.log(' _   _    _   _   _     ____ _____ _   _ ____ ___ ___\n| \\ | |  / \\ | | | |   / ___|_   _| | | |  _ \\_ _/ _ \\\n|  \\| | / _ \\| | | |   \\___ \\ | | | | | | | | | | | | |\n| |\\  |/ ___ \\ |_| |    ___) || | | |_| | |_| | | |_| |\n|_| \\_/_/   \\_\\___/    |____/ |_|  \\___/|____/___\\___/');
-}());
+
+    // monitor scroll and add page header
+    var body = doc.body,
+        headerVisible = false,
+        pageHeader = doc.getElementById('page-header');
+
+    doc.onscroll = function () {
+        var isVisible = body.scrollTop > 400;
+
+        console.log('onscroll', body.scrollTop, isVisible);
+
+        if (isVisible !== headerVisible) {
+            headerVisible = isVisible;
+
+
+            if (headerVisible) {
+                pageHeader.className = 'page-header page-header--visible';
+            } else {
+                pageHeader.className = 'page-header';
+            }
+        }
+    };
+
+}(document));
